21f7afc40d7d04c4fa0d08f1d32b7414b34c9f15
Min <borellanas@gmail.com>
2026-07-12 19:49:03 -0400

    Add files via upload

---
+++ src/test/java/cl/duoc/gatewayev/GatewayEvApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.gatewayev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayEvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
