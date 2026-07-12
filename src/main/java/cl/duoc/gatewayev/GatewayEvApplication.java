34c669d1bd36b0c4d63ddd301ea626c503bb6a34
Mincito <borellanas@gmail.com>
2026-07-12 19:45:24 -0400

    Creado el gateway

---
+++ src/main/java/cl/duoc/gatewayev/GatewayEvApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.gatewayev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayEvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayEvApplication.class, args);
+	}
+
+}
